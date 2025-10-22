4b509229844a4b1d32504085f7fad1b3e398e1a4
Sinoe_Ruiz <zs23004727@gmail.com>
2025-10-21 19:52:09 -0600

    feat(backend): scaffold Spring Boot project from NetBeans Initializr

---
+++ backend/src/test/java/com/sinoe/authmfa/AuthMfaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinoe.authmfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMfaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
